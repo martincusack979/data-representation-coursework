114a81666193dc6ed0b78851c4438be4283d87a4
martincusack979 <cusackmartin@gmail.com>
2023-10-04 21:55:25 +0100

    data rep lab1

--- labs/import requests/import requests.py
+++ labs/import requests/import requests.py
@@ -1,6 +1,14 @@
 import requests
+import csv
+
+from xml.dom.minidom import parse, parseString
+
+url: "http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML"
+
 
 page = requests.get("http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML")
-print(page)
-print("------------")
-print(page.content)
+doc = parseString(page.content)
+# check that it works
+# print(doc.toprettyxml())
+
+
