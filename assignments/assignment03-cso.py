f49c9058cf9a713c42452a5b77f01a1f6f96cd0c
martincusack979 <cusackmartin@gmail.com>
2023-10-25 16:03:14 +0100

    assignment 03

---
+++ assignments/assignment03-cso.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+# find "exchequer account (historical series)"" from CSO
+url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en"
+
+def getAll():   
+    response = requests.get(url)
+    return response.json()
+
+if __name__ == "__main__":
+    with open("exchequer.json", "wt") as fp:
+        print(json.dumps(getAll()), file=fp)
+
+
+    
+
+
+
+
