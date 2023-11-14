e2d6a2661335f3788503086a9eea1424cc7de838
martincusack979 <cusackmartin@gmail.com>
2023-11-14 21:47:03 +0000

    lab 5 github API

--- labs/lab5.py
+++ labs/lab5.py
@@ -1,15 +1,8 @@
 import requests
 import urllib.parse
-import configparser
-from config import config as cfg
 
 targetUrl = "https://en.wikipedia.org"
-apiKey = cfg['rtQ6A9S9j89Avc6e7gPCxbTPuFyXBifC26iPkmuFFddZjA7ERbhkq3pwwbOI4CkJ']
-
-
-config = {
-"htmltopdfkey":"eAs3mWn7R59gxZ6kSYysYdpTUsivY7Am74aSN36pOHdMMyTu3XDpdSOtaAySz3qF"
-}
+apiKey = 'rtQ6A9S9j89Avc6e7gPCxbTPuFyXBifC26iPkmuFFddZjA7ERbhkq3pwwbOI4CkJ'
 
 apiurl = 'https://api.html2pdf.app/v1/generate'
 
@@ -21,5 +14,6 @@ response = requests.get(requestUrl)
 print (response.status_code)
 
 result =response.content
-with open("document.pdf", "wb") as handler:
+with open("wiki.pdf", "wb") as handler:
     handler.write(result)
+
