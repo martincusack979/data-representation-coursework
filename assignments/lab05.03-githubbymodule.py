cde9cc1f857484d679b5d6ef720675335f456018
martincusack979 <cusackmartin@gmail.com>
2023-12-21 21:04:39 +0000

    lab5.3

---
+++ assignments/lab05.03-githubbymodule.py
@@ -0,0 +1,18 @@
+import requests
+import json
+from github import Github
+from config import config as cfg
+
+filename = "repos-assignment4.json"
+
+url = 'https://api.github.com/repos/martincusack979/assignment5'
+
+apikey = cfg["githubkey"]
+response = requests.get(url, auth = ('token', apikey))
+
+print (response.status_code)
+#print (response.json())
+
+with  open(filename, 'w') as fp:
+    repoJSON = response.json()
+    json.dump(repoJSON, fp, indent=4)
--- labs/config.py
+++
@@ -1,3 +0,0 @@
-config = {
-    "githubkey": "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"
-}
