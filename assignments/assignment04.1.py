75d98852048ff16a804e474634d8103a59c4ee5e
martincusack979 <cusackmartin@gmail.com>
2023-12-22 15:14:48 +0000

    assignment 4 failed attempt

--- assignments/assignment04.1.py
+++ assignments/assignment04.1.py
@@ -5,6 +5,8 @@ from config import config as cfg
 
 filename = "repos-assignment4.json"
 
+# create new repository for purposes of assignment named "assignment 4"
+
 url = 'https://api.github.com/repos/martincusack979/assignment4'
 
 apikey = cfg["githubkey"]
@@ -16,3 +18,20 @@ print (response.status_code)
 with  open(filename, 'w') as fp:
     repoJSON = response.json()
     json.dump(repoJSON, fp, indent=4)
+
+
+# get the download url of the file in "assignment 4" repository called replace.txt    
+fileinfo = repo.get contents("replace.txt")
+urlOfFile= fileInfo.download_url
+
+# make hhtp request to the file
+response = requests.get(urlOfFile)
+contentOfFile = response.text
+
+# Update the contents of the file on git up by using the function 
+
+gitHubResponse=repo.update_file(fileInfo.path,"updated by prog",
+newContents,fileInfo.sha)
+
+
+
